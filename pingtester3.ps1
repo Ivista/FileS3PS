c5e2c8103354eaca83f2fbdeb6ebbdaf7c4b40a0
Ivista <bevanc6@gmail.com>
2019-04-03 20:30:33 +0100

    pingtester3 added

---
+++ pingtester3.ps1
@@ -0,0 +1,38 @@
+﻿foreach ($server in (Get-Content c:\pscode\servers.txt)) {
+#skip blank lines
+if (($server).length -gt 0) 
+{        $pingresults = ping -n 2 $server
+
+$server
+
+     if ($pingresults -match "again") {
+
+        write-host "This PC is not recognised on the network"
+
+	
+	$server >>C:\pingtester\UnRecognisedMachines.txt
+
+
+               }
+			   
+			   
+
+
+
+     if ($pingresults -match "Reply") {
+
+        write-host "This PC is recognised on the network"
+        $Dave = Get-WmiObject -ComputerName $server -Class Win32_Product
+
+
+               }
+
+      ElseIf ($pingresults -match "timed")  {
+
+	write-host " Machine $hostname is not powered on"
+
+	
+	$server >>C:\pingtester\Notpoweredon.txt
+}
+} 
+}
