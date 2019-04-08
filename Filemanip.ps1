afaeb9d5bf5494391d42fef8d5afc5057d4206d6
BevDesk <bevanc6@gmail.com>
2019-04-08 19:29:28 +0100

    Latest version, first Prod version!

--- Filemanip.ps1
+++ Filemanip.ps1
@@ -6,6 +6,7 @@ function Write-Log {
     )
 
     #Calculate full path for log files
+
     $logPath = 'C:\amazon\TestLog.txt'
     
     #Calculate time stamp for log entry
@@ -17,30 +18,40 @@ function Write-Log {
 }
 
 #Main execution block
-
+#Set credentials and varibles
+$position = 0
+$vidOrpho =""
 Set-AWSCredential -ProfileName RayC
+
+#Collect path and vaidata
 $path = read-host "Enter path to files"
 
 $pathexists = Test-Path -Path $path 
+
 If ($pathexists -eq $false)
     { write-host 'Error with path!'
     write-log -data 'Error with path to files!'
     Break }
-write-host 'hello world'
-$vidOrpho = read-host "Enter 1 for Video or 2 for photos"
+
+
+
+
+#Select Video or Photo
 while ("1", "2" -notcontains $vidOrpho) {
         $vidOrpho = read-host "Enter 1 for Video or 2 for photos"
     }
 
-if ($vidOrpho = 1 ) {
+if ($vidOrpho -eq '1' ) {
 
     $filetype = '*.mp4'
+    $bucket = 'bevvideo1'
 }
-elseif ($vidOrpho = 2) {
+elseif ($vidOrpho -eq '2') {
     
-    $filetype = '*.jpeg'
+    $filetype = '*.jpg'
+    $bucket = 'bevphoto1'
 }
-
+#Drill through file structure and collect vaid files
 try {
     $files = Get-ChildItem -Path $path -File -Recurse -Include $filetype -ErrorAction Stop
 }
@@ -51,24 +62,26 @@ catch {
 }
 
 
-
+#Display file count to be uploaded. 
 Write-host "Files to be uploaded: $($files.count)"
 $total = $files.count 
-write-host 'hello world'
+
+#Extract lastWritetime (Modified Date) from file 
 foreach ($file in $files)
 
 {
 try {
-    $mem = Get-ItemProperty -Path $file.FullName | Select-Object -ExpandProperty CreationTime 
+    $mem = Get-ItemProperty -Path $file.FullName | Select-Object -ExpandProperty LastWriteTime
 }
 catch {
     write-host "Error with file property for $($file.fullname)!"
     write-log -data "Error with file property for $($file.fullname)!"
 }
-
+#Extract year and add / at the end for AWS key reference. 
 [string]$keyyear =$mem.Year
 $keyyear = $keyyear.Insert(4,"/")
 
+#Exract month and match the number 1-12 to month with a switch. 
 $month = $mem.Month
 
 switch ( $month )
@@ -87,14 +100,29 @@ switch ( $month )
     12 { $monthres = 'December'}
     Default { $monthres = 'Unknown'}
 }
+#Add a / for AWS Key reference
+$monthres = $monthres + '/'
 
-$keyresult = $keyyear + $monthres
+# combine all varibles 
+$keyresult = $keyyear + $monthres + $file.Name
 
-Write-Progress -Activity "Copying data from '$path' to S3 Bucket" -Status "Copying File '$File'" -PercentComplete (($Position/$total)*100)
-read-host 'press enter'
-#Write-S3Object -BucketName bevcoatespower -file $file.FullName  -Key $keyresult
+#Command for upload to AWS key result loaded at this stage. 
+try {
+    Write-S3Object -BucketName $bucket -file $file.FullName  -Key $keyresult -ErrorAction Stop -Region eu-west-2
+    write-log -data "Success $($file.FullName) uploaded to S3 as $($keyresult)"
+}
+catch {
+    write-host "Error with file write to S3 for $($file.fullname)!"
+    write-log -data "Error with file write to S3 for $($file.fullname)!"
+}
+#Writes progress to a gui display while the script runs. 
 $Position++
+Write-Progress -Activity "Copying data '$path' to S3 Bucket" -Status "Copying File '$File'" -PercentComplete (($Position/$total)*100)
+write-host "File $($position) of $($total)"
+Start-Sleep -Seconds 1
 
 }
+
+Write-host 'Upload completed'
 
 
