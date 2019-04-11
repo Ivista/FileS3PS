5e9ada999e836db94d7e5fda998133d0b6ca058e
Ivista <bevanc6@gmail.com>
2019-04-11 21:28:10 +0100

    Upload of Git commands

---
+++ Git.ps1
@@ -0,0 +1,20 @@
+
+Pulls in the repository
+C:\gitrepo1>git clone https://github.com/Ivista/FileS3PS.git
+
+Shows what is different between us and what is in the cloud.  Use between git add and git commit to track changes
+Git status 
+
+To add files to project, CASE Sensitive!
+Git add Git.ps1
+
+Means locking it into the 'local!' repository.
+Git commit  -m "Upload of git commands"
+
+previous command locks in file to local repository.
+Need to sync this now up to git cloud.
+This does the sync, do below then check in browser that all is synced up with cloud. 
+git push
+
+You need to run the command within the actual repository on the local machine, same folder which has the .git in it!
+
